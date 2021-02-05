31a9d6032edac1747cb664d7c805aae00507695a
mjones1818 <michael.jones0532@gmail.com>
2021-02-05 13:03:24 -0600

    commit tree 23d787839518914bf871a2acb96fcf80869a16c2

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component {
+  state = {
+    timesClicked: 0
+  }
+
+  handleClick = event => {
+    this.setState(previousState => ({
+      timesClicked: previousState.timesClicked + 1
+    }))
+  }
+  
+  render() {
+    return(
+      <button onClick={this.handleClick}>{this.state.timesClicked}
+        </button>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,46 @@
-// Code YouTubeDebugger Component Here
+
+import React, {Component} from 'react'
+
+export default class YouTubeDebugger extends Component {
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
+      }
+    }
+  }
+  
+  handleClick = event => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  }
+
+  handleResolutionChange = event => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          resolution: '720p'
+        }
+      }
+    })
+  }
+
+
+  render() {
+    return(
+      <div>
+        <button className='bitrate' onClick={this.handleClick}>bitrate</button>
+        <button className='resolution' onClick={this.handleResolutionChange}>Resolution</button>
+      </div>
+      
+    )
+  }
+}
